66322c125b6d9f660a585a61af1a98ae17cbf915
SimonFV <sfv0215@gmail.com>
2024-11-25 22:45:37 -0600

    Added delays for fanout 3.

--- Tarea_1/synopsys_custom/custom_compiler.tcl
+++ Tarea_1/synopsys_custom/custom_compiler.tcl
@@ -1,1 +1,56 @@
 db::setAttr geometry -of [gi::getFrames 0] -value 600x300+648+382
+de::showOpenDesign
+gi::setActiveDialog [gi::getDialogs {deOpenDesign}]
+db::setAttr geometry -of [gi::getDialogs {deOpenDesign}] -value 645x491+628+268
+gi::setCurrentIndex {designLibs} -index {basic_cells} -in [gi::getDialogs {deOpenDesign}]
+gi::setItemSelection {designLibs} -index {basic_cells} -in [gi::getDialogs {deOpenDesign}]
+gi::setCurrentIndex {designCells} -index {test_inv} -in [gi::getDialogs {deOpenDesign}]
+gi::setItemSelection {designCells} -index {test_inv} -in [gi::getDialogs {deOpenDesign}]
+gi::setCurrentIndex {designCells} -index {test_inv_SPICE} -in [gi::getDialogs {deOpenDesign}]
+gi::setItemSelection {designCells} -index {test_inv_SPICE} -in [gi::getDialogs {deOpenDesign}]
+gi::setCurrentIndex {designCells} -index {inv} -in [gi::getDialogs {deOpenDesign}]
+gi::setItemSelection {designCells} -index {inv} -in [gi::getDialogs {deOpenDesign}]
+gi::setCurrentIndex {designViews} -index {schematic} -in [gi::getDialogs {deOpenDesign}]
+gi::setItemSelection {designViews} -index {schematic} -in [gi::getDialogs {deOpenDesign}]
+gi::pressButton {ok} -in [gi::getDialogs {deOpenDesign}]
+db::setAttr dockSize -of [gi::getAssistants dePropertyEditor -from [gi::getWindows 1]] -value 290x818
+db::setAttr maximized -of [gi::getFrames 1] -value true
+db::setAttr dockSize -of [gi::getAssistants dePropertyEditor -from [gi::getWindows 1]] -value 290x891
+db::setAttr geometry -of [gi::getFrames 1] -value 1910x1020+5+55
+de::zoom -window [gi::getWindows 1] -factor [db::getPrefValue deCanvasMouseZoomFactor] -center {0.65625 -0.6375}
+de::zoom -window [gi::getWindows 1] -factor [db::getPrefValue deCanvasMouseZoomFactor] -center {0.65625 -0.6375}
+de::zoom -window [gi::getWindows 1] -factor [expr {1.0/[db::getPrefValue deCanvasMouseZoomFactor]}] -center {0.6875 -0.50625}
+de::zoom -window [gi::getWindows 1] -factor [expr {1.0/[db::getPrefValue deCanvasMouseZoomFactor]}] -center {0.70625 -0.425}
+de::zoom -window [gi::getWindows 1] -factor [expr {1.0/[db::getPrefValue deCanvasMouseZoomFactor]}] -center {0.86875 0.4}
+de::zoom -window [gi::getWindows 1] -factor [db::getPrefValue deCanvasMouseZoomFactor] -center {0.86875 0.53125}
+de::zoom -window [gi::getWindows 1] -factor [expr {1.0/[db::getPrefValue deCanvasMouseZoomFactor]}] -center {1.06875 -1.2625}
+de::zoom -window [gi::getWindows 1] -factor [db::getPrefValue deCanvasMouseZoomFactor] -center {0.74375 0.59375}
+de::zoom -window [gi::getWindows 1] -factor [expr {1.0/[db::getPrefValue deCanvasMouseZoomFactor]}] -center {0.8125 0.25625}
+de::zoom -window [gi::getWindows 1] -factor [db::getPrefValue deCanvasMouseZoomFactor] -center {0.95 -0.20625}
+de::zoom -window [gi::getWindows 1] -factor [expr {1.0/[db::getPrefValue deCanvasMouseZoomFactor]}] -center {0.7875 -0.06875}
+de::zoom -window [gi::getWindows 1] -factor [db::getPrefValue deCanvasMouseZoomFactor] -center {0.80625 -0.2375}
+de::zoom -window [gi::getWindows 1] -factor [db::getPrefValue deCanvasMouseZoomFactor] -center {0.78125 -0.15}
+de::zoom -window [gi::getWindows 1] -factor [expr {1.0/[db::getPrefValue deCanvasMouseZoomFactor]}] -center {0.78125 -0.15}
+gi::executeAction giCloseWindow -in [gi::getWindows 1]
+gi::setActiveWindow 0
+gi::setActiveWindow 0 -raise true
+db::setAttr geometry -of [gi::getFrames 0] -value 600x300+1139+648
+de::showOpenDesign
+gi::setActiveDialog [gi::getDialogs {deOpenDesign}]
+db::setAttr geometry -of [gi::getDialogs {deOpenDesign}] -value 645x491+628+268
+gi::pressButton {ok} -in [gi::getDialogs {deOpenDesign}]
+db::setAttr dockSize -of [gi::getAssistants dePropertyEditor -from [gi::getWindows 2]] -value 290x891
+de::deselectAll [db::getNext [de::getContexts -window 2]]
+de::select [de::getActiveFigure [gi::getWindows 2] -point {0.725 0.46875} -index 0 -intent none]
+gi::setItemSelection {parameters} -index {w,all} -in [gi::getAssistants dePropertyEditor -from [gi::getWindows 2]]
+gi::setCurrentIndex {parameters} -index {w,Value} -in [gi::getAssistants dePropertyEditor -from [gi::getWindows 2]]
+gi::setField {parameters} -value {1.8u} -index {w,Value} -in [gi::getAssistants dePropertyEditor -from [gi::getWindows 2]]
+gi::executeAction dePropertyEditorApplyChanges -in [gi::getAssistants dePropertyEditor -from [gi::getWindows 2]]
+de::deselectAll [db::getNext [de::getContexts -window 2]]
+de::deselectAllLogic -context [de::getContexts -filter {%editDesign==[db::getAttr editDesign -of [db::getNext [de::getContexts -window 2]]]}]
+gi::executeAction giCloseWindow -in [gi::getWindows 2]
+gi::executeAction giCloseWindow -in [gi::getWindows 2]
+gi::setActiveWindow 0
+gi::setActiveWindow 0 -raise true
+db::setAttr geometry -of [gi::getFrames 0] -value 600x300+701+427
+gi::executeAction giCloseWindow -in [gi::getWindows 0]
