a9ff9906e97791bc6e01b6d0ea8f46237a2b7ff5
Simón Fallas <47420204+SimonFV@users.noreply.github.com>
2024-11-25 22:29:45 -0600

    Added python files.

--- Tarea_1/Hspice_files/short_circuit_current/short_circuit_current.py
+++ Tarea_1/Hspice_files/short_circuit_current/short_circuit_current.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Cargar datos desde archivo de texto
-data = pd.read_csv('short_circuit_current.mt0', delim_whitespace=True)
+data = pd.read_csv('short_circuit_current.csv', delim_whitespace=True)
 
 # Extraer datos relevantes
 w_p = data['w_p']  # Ancho del PMOS
@@ -10,7 +10,7 @@ current = data['current_at_vdd2']  # Corriente medida
 
 # Graficar
 plt.figure(figsize=(8, 6))
-plt.plot(w_p, current, marker='o', label='I vs w_p')
+plt.plot(w_p, -current, marker='o', label='I vs w_p')
 plt.title('Corriente vs Ancho PMOS (w_p)')
 plt.xlabel('w_p (nm)')
 plt.ylabel('Corriente (A)')
