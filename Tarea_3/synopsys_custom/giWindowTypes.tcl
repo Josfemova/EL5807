b75dba06972e217fa8d36420943dff5a9b09c22d
SimonFV <sfv0215@gmail.com>
2024-11-27 18:36:27 -0600

    Added delays 1 to 0 hold

--- Tarea_3/synopsys_custom/giWindowTypes.tcl
+++ Tarea_3/synopsys_custom/giWindowTypes.tcl
@@ -1,10 +1,13 @@
-set wt [gi::getWindowTypes dbFindReplace]
-db::setAttr wt.geometry -value "580x620"
+set wt [gi::getWindowTypes seSchematic]
+db::setAttr wt.geometry -value "1632x947+50+100"
+db::setAttr wt.maximized -value "false"
+set wt [gi::getWindowTypes giConsole]
+db::setAttr wt.geometry -value "600x300+5+699"
 db::setAttr wt.maximized -value "false"
 set wt [gi::getWindowTypes deBookmarkManager]
 db::setAttr wt.geometry -value "530x400"
 db::setAttr wt.maximized -value "false"
-set wt [gi::getWindowTypes giHomePage]
-db::setAttr wt.geometry -value "1014x710+5+52"
+set wt [gi::getWindowTypes dbFindReplace]
+db::setAttr wt.geometry -value "580x620"
 db::setAttr wt.maximized -value "false"
 unset wt
