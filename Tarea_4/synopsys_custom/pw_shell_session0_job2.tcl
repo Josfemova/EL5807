ed900108e2092b0ac1d09835258301dc0f8d86f2
SimonFV <sfv0215@gmail.com>
2024-12-01 12:25:05 -0600

    Power consumption of flip flop schem

---
+++ Tarea_4/synopsys_custom/pw_shell_session0_job2.tcl
@@ -0,0 +1,5 @@
+::sa::_backgroundSaeShell::signalProcessReady 1733073427 758442
+::wf::viewerExited
+::wf::viewerExited
+::wf::updateViewerLicense 2
+::sa::_backgroundSaeShell::exit
---
+++ Tarea_4/synopsys_custom/pw_shell_session7_job1.tcl
@@ -0,0 +1,23 @@
+::sa::_backgroundSaeShell::signalProcessReady 1733075873 758442
+::wf::updateViewerLicense 2
+::wf::updateViewerLicense 2
+::sa::_backgroundSaeShell::runScript /home/sfallas_II_2024_vlsi/simulation/tarea4,flip_flop_8b_test,schematic/bg_script.tcl 0 sa::_testSuite::createNetlist::netlistingComplete scriptDir /home/sfallas_II_2024_vlsi/simulation/tarea4,flip_flop_8b_test,schematic testSuiteName saeSession3 cmdArgs {FineSim_schem_power 1 1 0 0 0} taskId 6
+db::setAttr geometry -of [gi::getFrames 1] -value 1014x710+5+52
+::sa::_backgroundSaeShell::runScript /home/sfallas_II_2024_vlsi/simulation/tarea4,flip_flop_8b_test,schematic/bg_script.tcl 0 sa::_testSuite::createNetlist::netlistingComplete scriptDir /home/sfallas_II_2024_vlsi/simulation/tarea4,flip_flop_8b_test,schematic testSuiteName saeSession3 cmdArgs {FineSim_schem_power 1 1 0 0 0} taskId 7
+::sa::_backgroundSaeShell::runScript /home/sfallas_II_2024_vlsi/simulation/tarea4,flip_flop_8b_test,schematic/bg_script.tcl 0 sa::_testSuite::run::netlistingComplete scriptDir /home/sfallas_II_2024_vlsi/simulation/tarea4,flip_flop_8b_test,schematic testSuiteName saeSession3 cmdArgs {FineSim_schem_power 0 new {{}}} taskId 8
+::sa::_backgroundSaeShell::runScript /home/sfallas_II_2024_vlsi/simulation/tarea4,flip_flop_8b_test,schematic/history_1/simulation/FineSim_schem_power/FineSimPro/nominal/.bg_measurement_script.tcl 0 sa::_resultsDatabase::saeShellProcessingCompletedProc testSuiteName saeSession3 uniqueSessionName {} testbenchName FineSim_schem_power resultsDir /home/sfallas_II_2024_vlsi/simulation/tarea4,flip_flop_8b_test,schematic/history_1/simulation/FineSim_schem_power/FineSimPro/nominal/results testSuiteDir /home/sfallas_II_2024_vlsi/simulation/tarea4,flip_flop_8b_test,schematic/history_1/simulation/FineSim_schem_power/FineSimPro/nominal databaseName /home/sfallas_II_2024_vlsi/simulation/tarea4,flip_flop_8b_test,schematic/history_1/simulation/FineSim_schem_power/FineSimPro/nominal/resultsDatabase remoteRun {} testbenchId 1 saveImages true
+::wf::viewerExited
+::wf::viewerExited
+::wf::updateViewerLicense 2
+::sa::_backgroundSaeShell::runScript /home/sfallas_II_2024_vlsi/simulation/tarea4,flip_flop_8b_test,schematic/bg_script.tcl 0 sa::_testSuite::run::netlistingComplete scriptDir /home/sfallas_II_2024_vlsi/simulation/tarea4,flip_flop_8b_test,schematic testSuiteName saeSession3 cmdArgs {FineSim_schem_power 0 overwrite {{}}} taskId 9
+::sa::_backgroundSaeShell::runScript /home/sfallas_II_2024_vlsi/simulation/tarea4,flip_flop_8b_test,schematic/history_1/simulation/FineSim_schem_power/FineSimPro/nominal/.bg_measurement_script.tcl 0 sa::_resultsDatabase::saeShellProcessingCompletedProc testSuiteName saeSession3 uniqueSessionName {} testbenchName FineSim_schem_power resultsDir /home/sfallas_II_2024_vlsi/simulation/tarea4,flip_flop_8b_test,schematic/history_1/simulation/FineSim_schem_power/FineSimPro/nominal/results testSuiteDir /home/sfallas_II_2024_vlsi/simulation/tarea4,flip_flop_8b_test,schematic/history_1/simulation/FineSim_schem_power/FineSimPro/nominal databaseName /home/sfallas_II_2024_vlsi/simulation/tarea4,flip_flop_8b_test,schematic/history_1/simulation/FineSim_schem_power/FineSimPro/nominal/resultsDatabase remoteRun {} testbenchId 1 saveImages true
+::wf::viewerExited
+::wf::viewerExited
+::wf::updateViewerLicense 2
+::sa::_backgroundSaeShell::runScript /home/sfallas_II_2024_vlsi/simulation/tarea4,flip_flop_8b_test,schematic/bg_script.tcl 0 sa::_testSuite::run::netlistingComplete scriptDir /home/sfallas_II_2024_vlsi/simulation/tarea4,flip_flop_8b_test,schematic testSuiteName saeSession3 cmdArgs {FineSim_schem_power 0 overwrite {{}}} taskId 10
+::sa::_backgroundSaeShell::runScript /home/sfallas_II_2024_vlsi/simulation/tarea4,flip_flop_8b_test,schematic/history_1/simulation/FineSim_schem_power/FineSimPro/nominal/.bg_measurement_script.tcl 0 sa::_resultsDatabase::saeShellProcessingCompletedProc testSuiteName saeSession3 uniqueSessionName {} testbenchName FineSim_schem_power resultsDir /home/sfallas_II_2024_vlsi/simulation/tarea4,flip_flop_8b_test,schematic/history_1/simulation/FineSim_schem_power/FineSimPro/nominal/results testSuiteDir /home/sfallas_II_2024_vlsi/simulation/tarea4,flip_flop_8b_test,schematic/history_1/simulation/FineSim_schem_power/FineSimPro/nominal databaseName /home/sfallas_II_2024_vlsi/simulation/tarea4,flip_flop_8b_test,schematic/history_1/simulation/FineSim_schem_power/FineSimPro/nominal/resultsDatabase remoteRun {} testbenchId 1 saveImages true
+::wf::viewerExited
+::wf::viewerExited
+::wf::updateViewerLicense 2
+::sa::_backgroundSaeShell::parentRunning
+::sa::_backgroundSaeShell::exit
